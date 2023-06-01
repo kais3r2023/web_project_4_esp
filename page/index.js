4b51cfc58cd837feb8272fd23a9304f6e62ac296
kais3r2023 <122757568+kais3r2023@users.noreply.github.com>
2023-05-31 23:36:22 -0500

    validacion de formularios

--- page/index.js
+++ page/index.js
@@ -145,7 +145,7 @@ initialCards.forEach((card,index)=>{
 /* funciones para añadir titulo e imagenes nuevas */
 
 
-const btnPlaceSave = document.querySelector("#formulary-place");
+const formularyPlace = document.querySelector("#formulary-place");
   
 
 /* funcion para agregar nuevas tarjetas */
@@ -161,10 +161,10 @@ function addNewCard(event){
   gallery.appendChild(cardComplete);
   
   handlerDisplayPopUpPlace();
-  btnPlaceSave.reset();
+  formularyPlace.reset();
 };
 
-btnPlaceSave.addEventListener('submit', addNewCard);
+formularyPlace.addEventListener('submit', addNewCard);
 
 /* Funcion para dar modificar corazon de like */
 
@@ -188,3 +188,57 @@ function zoomImgPopUp(link, name){
   zoomImgSrc.src = link;
   zoomTitle.innerHTML = name;
 };
+
+/* Validacion de formularios */
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const btnDisable = formElement.querySelector(".formulary__save-button");
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add("formulary__data-error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("formulary__data-error_active");
+  btnDisable.classList.add("formulary__save-button_disable");
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const btnDisable = formElement.querySelector(".formulary__save-button");
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove("formulary__data-error");
+  errorElement.classList.remove("formulary__data-error_active");
+  errorElement.textContent = "";
+  btnDisable.classList.remove("formulary__save-button_disable");
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+    formularyProfile.removeEventListener('submit', handlerProfile);
+    formularyPlace.removeEventListener('submit', addNewCard);
+  } else {
+    hideInputError(formElement, inputElement);
+    formularyProfile.addEventListener('submit', handlerProfile);
+    formularyPlace.addEventListener('submit', addNewCard);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".formulary__data"));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(".formulary"));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });
+
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
